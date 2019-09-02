8219ff71e7fe912afb0a16cd56a6c95fd6e8e6c8
yosuke <yosuke.watanabe.0923@gmail.com>
2019-09-02 13:41:58 +0900

    add comment: kaguya0

--- src/ipadic_control.py
+++ src/ipadic_control.py
@@ -1,6 +1,7 @@
 import pprint
 import re
 
+
 class Word():
     __slots__ = ['word', 'genre', 'katsuyo']
 
@@ -14,6 +15,7 @@ class Word():
 
     def __repr__(self):
         return f'{self.word} {self.genre} {self.katsuyo}'
+
 
 dic_list = {
     'Adj.dic': [],  # 形容詞
@@ -44,6 +46,7 @@ dic_list = {
     'Symbol.dic': [],  # 記号
     'Verb.dic': [],  # 動詞
 }
+
 
 def verb_classify(t):
     # 活用型の名前の変換
@@ -76,37 +79,47 @@ def verb_classify(t):
     else:
         return 'UNKNOWN'
 
-def load_dic(dic_path):
-    # dicファイルをWord型のリストに変換
+
+def load_dic(dic_list):
+    # dicファイルをWord型のリストに変換してdic_listに追加
     def dict_parser(s):
         GENRE = s[s.find('(', s.find('品詞'))+1: s.find(')',s.find('品詞'))].replace(' ','-')
         WORD = s[s.find('(', s.find('見出し語'))+1: s.find(')',s.find('見出し語'))].split(' ')[0]
         KATSUYO = s[s.find(' ', s.find('活用型'))+1: s.find(')', s.find('活用型'))] if '活用型' in s else ''
         return Word(WORD, GENRE, KATSUYO)
 
-    out = []
-    with open(dic_path, mode='r', encoding='euc_jp') as f:
-        for s in f.readlines():
-            w = dict_parser(s)
-            out.append(w)
-    return out
+    for path in dic_list:
+        with open('./ipadic-2.7.0/'+path, mode='r', encoding='euc_jp') as f:
+            for s in f.readlines():
+                w = dict_parser(s)
+                dic_list[path].append(w)
 
-def noun_hiragana_check(dic_list):
+
+def hiragana_check(dic_list, genre):
     def len_of_word(w):
         return len(str(w))
 
-    only_hiragana = []
-    with_hiragana = []
-    for path in dic_list:
-        dic_list[path] = load_dic('./ipadic-2.7.0/'+path)
-        for e in dic_list[path]:
-            if re.compile('[あ-ん]+').fullmatch(str(e)) and '名詞' in e.genre:
-                only_hiragana.append(e)
-            elif re.search(r'[あ-ん]', str(e)) and '名詞' in e.genre:
-                with_hiragana.append(e)
-    return only_hiragana.sort(key=len_of_word), with_hiragana.sort(key=len_of_word)
+    hira_only = []
+    hira_mix = []
+    for key in dic_list:
+        for e in dic_list[key]:
+            if re.compile('[あ-ん]+').fullmatch(str(e)) and genre in e.genre:
+                hira_only.append(e)
+            elif re.search(r'[あ-ん]', str(e)) and genre in e.genre:
+                hira_mix.append(e)
+    hira_only.sort(key=len_of_word)
+    hira_mix.sort(key=len_of_word)
+    with open(f'hira_only_of_{genre}.txt', mode='w') as f:
+        for p in hira_only:
+            f.write(repr(p)+'\n')
+    with open(f'hira_mix_of_{genre}.txt', mode='w') as f:
+        for p in hira_mix:
+            f.write(repr(p)+'\n')
+    print(f'ひらがなのみの{genre}の数：{len(hira_only)}')
+    print(f'ひらがな混在の{genre}の数：{len(hira_mix)}')
 
-def get_verb_dic(dic_list):
+
+def gen_verb(dic_list):
     d = {
         "VERB5KA": [],
         "VERB5SA": [],
@@ -126,12 +139,9 @@ def get_verb_dic(dic_list):
     for p in dic_list['Verb.dic']:
         path_name = verb_classify(p.katsuyo)
         d[path_name].append(p)
-    return d
 
-def gen_verb(dic_list):
-    d = get_verb_dic(dic_list)
     for k in d:
-        with open(f'./dic/verbs/{k}.txt', mode='w') as f:
+        with open(f'./grammar/dic/verbs/{k}.txt', mode='w') as f:
             s = ''
             for p in d[k]:
                 if not (str(p) in ['行く', '来る', 'くる', 'する'] or len(str(p)) < 2 or re.match('[あ-ん]{2}$', str(p))):
@@ -141,4 +151,39 @@ def gen_verb(dic_list):
                         s += str(p)[:-1]+'\n'
             f.write(s)
 
-gen_verb(dic_list)
+
+def generate(dic_list):
+    gen_verb(dic_list)
+    with open(f'grammar/dic/ADJ.txt', mode='w') as f:
+        s = ''
+        for p in dic_list['Adj.dic']:
+            s += str(p)[:-1] + '\n'
+        for p in dic_list['Noun.nai.dic']:
+            s += str(p)+'な\n'
+        f.write(s)
+    with open(f'grammar/dic/ADJV.txt', mode='w') as f:
+        s = ''
+        for p in dic_list['Noun.adjv.dic']:
+            s += str(p) + '\n'
+        f.write(s)
+    with open(f'grammar/dic/ADNM.txt', mode='w') as f:
+        s = ''
+        for p in dic_list['Adnominal.dic']:
+            s += str(p) + '\n'
+        f.write(s)
+    with open(f'grammar/dic/CONJ.txt', mode='w') as f:
+        s = ''
+        for p in dic_list['Conjunction.dic']:
+            s += str(p) + '\n'
+        f.write(s)
+    with open(f'grammar/dic/ADV.txt', mode='w') as f:
+        s = ''
+        for p in dic_list['Adverb.dic']:
+            s += str(p) + '\n'
+        f.write(s)
+
+
+load_dic(dic_list)
+generate(dic_list)
+
+
---
+++ src/tester.py
@@ -0,0 +1,40 @@
+import sys
+import re
+from pegpy.main import *
+
+
+def txt2array(path):
+    s = ''
+    with open(path, mode='r', encoding='utf_8') as f:
+        s = f.read()
+    s = re.sub(r'[ ]', '', s)
+    s = re.sub(r'[。]', '。\n', s)
+    return list(filter(lambda x: not x == '', s.split('\n')))
+
+
+def main(argv):
+    GRAMMAR = 'grammar/kaguya0.tpeg'
+    options = parse_options(['-g', GRAMMAR])
+    peg = load_grammar(options)
+    parser = generator(options)(peg, **options)
+    results = {
+        'ok': [],
+        'err': [],
+    }
+    lines = txt2array(argv[1])
+    for count,line in enumerate(lines):
+        sys.stdout.write(f'\rNow Processing: {count+1}/{len(lines)}')
+        tree = repr(parser(line))
+        if tree.startswith('[#err'):
+            results['err'].append((line, tree))
+        else:
+            results['ok'].append((line, tree))
+        sys.stdout.flush()
+    err_rate = 'ERR_RATE: %d/%d' % len(results['err']), len(lines)
+    print(err_rate)
+
+
+
+if __name__ == "__main__":
+    # python src/tester.py test_text/hoge.txt
+    main(sys.argv)
