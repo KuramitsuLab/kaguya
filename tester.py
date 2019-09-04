fbca561b708c14b9d78c136d6414a24f6cdf700c
yosuke <yosuke.watanabe.0923@gmail.com>
2019-09-04 22:28:56 +0900

    update: kaguya, tester

--- tester.py
+++ tester.py
@@ -1,8 +1,13 @@
 import sys
+import time
+from pathlib import Path
+import datetime
 import re
 from pegpy.main import *
 
+
 GRAMMAR = 'kaguya0.tpeg'
+
 
 def txt2array(path):
     s = ''
@@ -11,6 +16,33 @@ def txt2array(path):
     s = re.sub(r'[ ]', '', s)
     s = re.sub(r'[。]', '。\n', s)
     return list(filter(lambda x: not x == '', s.split('\n')))
+
+
+def logging(fn, aopl, pt, fr):
+    s = ''
+    date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    with open('test/result/LOG.md', mode='a', encoding='utf_8') as f:
+        s += f'+ Date: {date}\n'
+        s += f'+ Target File Name: `{fn}`\n'
+        s += f'+ Amount of Parse Lines: {aopl}\n'
+        s += f'+ Parse Required Time: {round(pt, 5)} sec\n'
+        s += f'+ Fail Rate: {fr[0]}/{fr[1]}\n'
+        s += '---\n\n'
+        f.write(s)
+
+
+def write_result(fpath, results, count=0):
+    try:
+        with open(fpath, mode='x', encoding='utf_8') as f:
+            s = ''
+            for text, tree in results:
+                s += text + '\n'
+                s += tree + '\n'
+                s += '\n'
+            f.write(s)
+    except FileExistsError:
+        fpath = fpath[:(fpath.rfind('.'))] + f'_{count}.txt'
+        write_result(fpath, results, count+1)
 
 
 def test(argv):
@@ -22,22 +54,30 @@ def test(argv):
         'fail': [],
     }
 
-    lines = txt2array(argv[1])
+    str_list = txt2array(argv[1])
+    FILE_NAME = argv[1][(argv[1].rfind('/'))+1 : (argv[1].rfind('.'))]
+    MAX_COUNT = int(argv[2]) if len(argv) > 2 else len(str_list)
+    if not Path('test/result').exists():
+        Path('test/result').mkdir()
 
-    for count,line in enumerate(lines):
-        sys.stdout.write(f'\rNow Processing: {count+1}/{len(lines)}')
-        tree = repr(parser(line))
-        if tree.startswith('[#err'):
-            results['fail'].append((line, tree))
+    START = time.time()
+    for count,s in enumerate(str_list):
+        if count >= MAX_COUNT:break
+        sys.stdout.write(f'\rNow Processing: {count+1}/{MAX_COUNT}')
+        tree = parser(s)
+        if 'Syntax Error' in repr(tree):
+            results['fail'].append((s, f'Remain: [{str(tree)}]'))
         else:
-            results['success'].append((line, tree))
+            results['success'].append((s, repr(tree)))
         sys.stdout.flush()
-    fail_rate = '\nERR_RATE: %d/%d' % (len(results['fail']), len(lines))
-    with open(argv[1][:-4]+'_fail.txt', mode='w') as f:
-        f.write('\n'.join(list(map(lambda tpl: str(tpl[0])+'\n'+str(tpl[1])+'\n', results['fail']))))
-    with open(argv[1][:-4]+'_success.txt', mode='w') as f:
-        f.write('\n'.join(list(map(lambda tpl: str(tpl[0])+'\n'+str(tpl[1])+'\n', results['success']))))
-    print(fail_rate)
+    STOP = time.time()
+
+    print()
+    FAIL_RATE = (len(results['fail']), MAX_COUNT)
+    write_result(f'test/result/{FILE_NAME}_success.txt', results['success'])
+    write_result(f'test/result/{FILE_NAME}_fail.txt', results['fail'])
+    logging(argv[1], MAX_COUNT, STOP-START, FAIL_RATE)
+
 
 
 if __name__ == "__main__":
