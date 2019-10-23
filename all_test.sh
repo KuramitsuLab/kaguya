aacd45248b369ee55c4dde8bb604cef06340ec0b
yosuke <yosuke.watanabe.0923@gmail.com>
2019-10-23 17:29:10 +0900

    fix

--- all_test.sh
+++ all_test.sh
@@ -7,16 +7,27 @@ mkdir test_${TARGET}/
 # gakkou.tpeg
 python tester.py test/${TARGET}.txt gakkou.tpeg False y
 
-cp test/result/${TARGET}.txt test_${TARGET}/ver_gakkou.txt
+mv test/result/${TARGET}.txt test_${TARGET}/result_ver_gakkou.txt
+mv test/result/${TARGET}_for_compare.txt test_${TARGET}/for_compare_ver_gakkou.txt
 cp gakkou.tpeg test_${TARGET}/gakkou.tpeg
 
-mv graph/${TARGET} test_${TARGET}/graph/ver_gakkou
+mv graph_${TARGET} test_${TARGET}/graph_ver_gakkou
 
 
 # kaguya.tpeg
 python tester.py test/${TARGET}.txt kaguya0.tpeg False y
 
-cp test/result/${TARGET}.txt test_${TARGET}/ver_kaguya.txt
+mv test/result/${TARGET}.txt test_${TARGET}/result_ver_kaguya.txt
+mv test/result/${TARGET}_for_compare.txt test_${TARGET}/for_compare_ver_kaguya.txt
 cp kaguya0.tpeg test_${TARGET}/kaguya0.tpeg
 
-mv graph/${TARGET} test_${TARGET}/graph/ver_kaguya
+mv graph_${TARGET} test_${TARGET}/graph_ver_kaguya
+
+# kaguya-kai.tpeg
+python tester.py test/${TARGET}.txt kaguya0-kai.tpeg False y
+
+mv test/result/${TARGET}.txt test_${TARGET}/result_ver_kaguya-kai.txt
+mv test/result/${TARGET}_for_compare.txt test_${TARGET}/for_compare_ver_kaguya-kai.txt
+cp kaguya0-kai.tpeg test_${TARGET}/kaguya0-kai.tpeg
+
+mv graph_${TARGET} test_${TARGET}/graph_ver_kaguya-kai
--- tester.py
+++ tester.py
@@ -175,7 +175,7 @@ def gen_dot(input_str, d):
             if unicodedata.east_asian_width(c) in ['W', 'F', 'H']:
                 bt = ', labelloc = "bottom"'
                 break
-        label = '' if v["label"] == '' else f' [label = "{v["label"]}"]'
+        label = '' if v["label"] == 'None' else f' [label = "{v["label"]}"]'
         def_node += f'    n_{k} [label = "{v["tag"]}"{bt}];\n'
         def_edge += '' if v["parent"] == '' else f'    n_{v["parent"]} -> n_{k}{label};\n'
     return template(input_str, def_node, def_edge)
@@ -218,12 +218,53 @@ def gen_graph(input_str, ast_str, path='graph.png'):
     Path(GEN_DOT_PATH).unlink()
 
 
+def leaf2token(word, nodes):
+    NON_UTILIZE = {
+        'Noun': '名詞',
+        'Block': '名詞',
+        'Adnominal': '連体詞',
+        'Adverb': '副詞',
+        'Conjunction': '接続詞',
+        'Interjection': '感動詞',
+    }
+    if   nodes[-1].startswith('ADJV'):
+        return f'{word} (形容動詞)'
+    elif nodes[-1].startswith('ADJ'):
+        return f'{word} (形容詞)'
+    elif nodes[-1].startswith('P_'):
+        return f'{word} (助詞)'
+    elif nodes[-1].startswith('AUX'):
+        return f'{word} (助動詞)'
+    elif nodes[-1].startswith('V'):
+        return f'{word} (動詞)'
+    else:
+        return f'{word} ({NON_UTILIZE[nodes[-1]]})'
+
+
+def gen_compare(ast):
+    def make_words(tree, words, nodes):
+        if len(tree.subs()) == 0:
+            leaf = str(tree)
+            current = [tree.tag] if tree.tag != '' else []
+            words.append(leaf2token(leaf, nodes+current))
+        else:
+            for i, (_, child) in enumerate(tree.subs()):
+                current = [tree.tag] if tree.tag != '' else []
+                make_words(child, words, nodes+current)
+
+    words = []
+    make_words(ast, words, [])
+    words.append('。 (文末)')
+    return words
+
+
 # main tester functions
-def test(target_name, grammar='kaguya0.tpeg', print_log='True'):
+def test(target_name, grammar='kaguya0.tpeg', print_log='True', _='y or n'):
     options = parse_options(['-g', grammar])
     peg = load_grammar(options)
     parser = generator(options)(peg, **options)
     results = []
+    compare_words = []
     fail_cnt = 0
 
     input_list = txt2array(target_name)
@@ -236,34 +277,39 @@ def test(target_name, grammar='kaguya0.tpeg', print_log='True'):
         try:
             tree = parser(s)
         except Exception as e:
-            pass
-        if 'Syntax Error' in repr(tree):
-            results.append((False, count+1, s, str(tree)))
+            print(e)
+        if tree.tag == 'err':
+            results.append((False, count+1, s, tree.inputs[tree.epos:]))
             fail_cnt += 1
         else:
             results.append((True, count+1, s, repr(tree)))
+            compare_words += gen_compare(tree)
         sys.stdout.flush()
     print()
     write_result(f'test/result/{FILE_NAME}.txt', results)
+    with open(f'test/result/{FILE_NAME}_for_compare.txt', mode='w', encoding='utf_8') as f:
+        f.write('\n'.join(compare_words))
     if not print_log in ['0', 'false', 'False']:
         print_err(results)
     print('Fail Rate: %d / %d' % (fail_cnt, len(input_list)))
     return results
 
 
-def test_with_graph(target_file, grammar_file, print_log='True'):
+def test_with_graph(target_file, grammar_file, print_log='True', _='y or n'):
     if not shutil.which('dot'):
         print('Not find "dot" command')
         print('Please install "Graphviz"')
         sys.exit()
     FILE_NAME = target_file[target_file.rfind('/')+1: target_file.rfind('.')]
     results = test(target_file, grammar_file, print_log)
-    succs = list(filter(lambda l: l[0], results))
-    MAX_COUNT = len(succs)
-    make_dir(f'graph/{FILE_NAME}')
-    for count, (bl, ln, s, ast) in enumerate(succs):
+    MAX_COUNT = len(results)
+    make_dir(f'graph_{FILE_NAME}')
+    for count, (bl, ln, s, ast) in enumerate(results):
         sys.stdout.write(f'\rNow Processing: {count+1}/{MAX_COUNT}')
-        gen_graph(escape(s), ast, f'graph/{FILE_NAME}/{ln}.png')
+        if bl:
+            gen_graph(escape(s), ast, f'graph_{FILE_NAME}/{count}.png')
+        else:
+            gen_graph(escape(s), f'[#Remain \'{escape(ast)}\']', f'graph_{FILE_NAME}/{count}.png')
         sys.stdout.flush()
     print()
 
