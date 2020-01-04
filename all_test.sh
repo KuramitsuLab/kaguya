a58a685b6179ee9c62c2a552b9c76c68a4157a23
xps <yosuke.watanabe.0923@gmail.com>
2020-01-04 13:16:44 +0900

    update

--- all_test.sh
+++ all_test.sh
@@ -11,13 +11,13 @@ grammars=(
 )
 
 for g in "${grammars[@]}" ; do
-    python tester.py test/${TARGET}.txt ${g}.tpeg False n
+    python tester.py -t test/${TARGET}.txt -g ${g}.tpeg -Graph
 
-    mv test/result/${TARGET}.txt test_${TARGET}/result_ver_${g}.txt
-    mv test/result/${TARGET}_for_compare.txt test_${TARGET}/for_compare_ver_${g}.txt
+    mv test/result/${TARGET}_${g}.txt test_${TARGET}/result_ver_${g}.txt
+    # mv test/result/${TARGET}_for_compare.txt test_${TARGET}/for_compare_ver_${g}.txt
     cp ${g}.tpeg test_${TARGET}/${g}.tpeg
 
-    mv graph_${TARGET} test_${TARGET}/graph_ver_${g}
-    python compare.py test/python_ans-edit.txt test_${TARGET}/for_compare_ver_${g}.txt > test_${TARGET}/diff_${g}.txt
+    mv graph_${TARGET}_${g} test_${TARGET}/graph_ver_${g}
+    # python compare.py test/python_ans-edit.txt test_${TARGET}/for_compare_ver_${g}.txt > test_${TARGET}/diff_${g}.txt
 done
 
